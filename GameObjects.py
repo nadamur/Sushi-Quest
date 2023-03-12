f52936c37a0cd9925af57ad68e8683d6dc0b59cf
nadamur <nmurad4@uwo.ca>
2023-03-12 18:35:46 -0400

    added movement

--- GameObjects.py
+++ GameObjects.py
@@ -59,20 +59,68 @@ class Actor(GameObjects):
 
     #Note to Neda: Feel free to change the move method to whatever you want. I just wanted to get something working.
 
-    
-    #to shirley from neda: hi yes im just gonna add double jumping as a movement that will require a boolean (unlocked) to be used
+
     #side note: we probably need to run this in the main class under 'while running' since it has to be permanently happening
+    
     def move(self):
+        #will keep track of whether the player is currently mid-air
+        isJump = False
+        #v is the velocity of the jump
+        v = 5
+        #m is just for the sake of keeping track of gravity
+        m = 1
+        #self explanatory
+        jumpCounter = 0
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
             self.rect.x -= self.speed
         if keys[pygame.K_RIGHT]:
             self.rect.x += self.speed
-        if keys[pygame.K_UP]:
-            self.rect.y -= self.speed
-        if keys[pygame.K_DOWN]:
-            self.rect.y += self.speed
+
+        #jumping
+        if self.skillLvl == 0:
+            if isJump ==False:
+                if keys[pygame.K_SPACE]:
+                   isJump = True
+            if isJump:
+                #F is the force
+                F = (1/2)*m*(v**2)
+                #we need to properly clean up the Player class before this method can work
+                y_position = self.get_position()
+                v = v-1
+                if v<0:
+                    m=-1
+                if v==-6:
+                    isJump= False
+
+                    #reset
+                    v=5
+                    m=1
+
+        if self.skillLvl>0:
+            if isJump ==False and jumpCounter<=2:
+                if keys[pygame.K_SPACE]:
+                   isJump = True
+                   jumpCounter+=1
+            if isJump:
+                #F is the force
+                F = (1/2)*m*(v**2)
+                #we need to properly clean up the Player class and position variable before this method can work
+                self.position.y -= F
+                v = v-1
+                if v<0:
+                    m=-1
+                if v==-6:
+                    jumpCounter=0
+                    isJump= False
+                    #reset
+                    v=5
+                    m=1
+        pygame.time.delay(10)
+        pygame.display.update()    
+
         
+
 
     #Note to Neda: We should have a skill method or a skill class for the double jump and everything? 
 
