1130001b47b6791ec22547e54b22ba7b7ab743c3
nadamur <nmurad4@uwo.ca>
2023-04-03 17:53:42 -0400

    fixed error

--- GameObjects.py
+++ GameObjects.py
@@ -73,7 +73,7 @@ class Star(pygame.sprite.Sprite):
     def __init__(self, x,y,direction):
         pygame.sprite.Sprite.__init__(self)
         self.speed = 10
-        self.image = pygame.image.load('groupproject-team-8/Sprites/temp_star.jpg')
+        self.image = pygame.image.load('Sprites/temp_star.jpg')
         self.rect = self.image.get_rect()
         self.rect.center = (x,y)
         self.direction = direction
